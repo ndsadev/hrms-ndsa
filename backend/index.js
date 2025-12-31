acfb3b839548d7d30f4cc2544b9cddcba29fa11b
ndsadev <ndsa.dev@gmail.com>
2025-12-31 17:50:01 +0530

    working on index.js file

--- backend/index.js
+++ backend/index.js
@@ -38,9 +38,7 @@ app.use(
 );
 
 // Logger
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+app.use(morgan("dev"));
 
 // Body Parser
 app.use(express.json({ limit: "20mb" }));
